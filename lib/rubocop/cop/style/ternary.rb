2437661e632983a42ebf05f7044c3ee0b7ece258
David Stosik <david.stosik+git-noreply@gmail.com>
2019-06-12 16:33:23 +0900

    Add Style/Ternary cop

---
+++ lib/rubocop/cop/style/ternary.rb
@@ -0,0 +1,27 @@
+module RuboCop
+  module Cop
+    module Style
+      class Ternary < Cop
+	# This cop checks for ternary op expressions.
+	#
+	# @example
+	#   # bad
+	#   cond ? a : b
+	#
+	#   # good
+        #   if cond
+        #     a
+        #   else
+        #     b
+        #   end
+        MSG = "Avoid the ternary operator completely. Use `if` or `unless` instead."
+
+        def on_if(node)
+          if node.ternary?
+            add_offense(node)
+          end
+        end
+      end
+    end
+  end
+end
