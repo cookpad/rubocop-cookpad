1fe6576ef161eb872eedf8098d73e6071f84a50a
David Stosik <david.stosik+git-noreply@gmail.com>
2019-06-12 15:16:57 +0900

    bundle gem rubocop-cookpad

---
+++ lib/rubocop/cookpad.rb
@@ -0,0 +1,8 @@
+require "rubocop/cookpad/version"
+
+module Rubocop
+  module Cookpad
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rubocop/cookpad/version.rb
@@ -0,0 +1,5 @@
+module Rubocop
+  module Cookpad
+    VERSION = "0.1.0"
+  end
+end
